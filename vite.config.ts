6576014cc57de3eb9f5f23b4a0aca0351226d57c
quietcrane <jinhelee@126.com>
2026-03-29 14:14:37 +0800

    fix: 适配Vercel部署

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,4 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
-      },
-    },
-  },
 })
